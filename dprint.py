8a9c09359f03522ea37e5a520b076a00abde5476
Mud <mudkippzs@gmail.com>
2022-02-16 23:31:19 +0000

    basic debug logger.

---
+++ dprint.py
@@ -0,0 +1,16 @@
+import argparse
+import pprint
+import datetime
+
+
+def dprint(message:[list, dict, str, int, bool] = None, debug_identifier:str = "Err: No Identifier set"):
+	now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S:")
+	base_message = f"[DEBUG]@[ {now}]>[{debug_identifier}])"
+	if type(message) in ["list", "dict"]:
+		print(base_message)
+		pp(message)
+	else:
+		print(base_message + "\t" + message)
+
+if __name__ == "__main__":
+	dprint("Debugger Printer Module Test Message.")
