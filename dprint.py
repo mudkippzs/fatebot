cc29c399d225ea3aeaad9938bc41c3a268891013
Mud <mudkippzs@gmail.com>
2022-02-17 19:02:37 +0000

    Converted to a class.

--- dprint.py
+++ dprint.py
@@ -2,16 +2,20 @@ import argparse
 import pprint
 import datetime
 
-def dprint(message:[list, dict, str, int, bool] = None, debug_identifier:str = "Err: No Identifier set"):
-	now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S:")
-	base_message = f"\n[DEBUG]@[ {now}]>[{debug_identifier}])\n"
-	message = str(message)
 
-	if type(message) in ["list", "dict"]:
-		print(base_message)
-		pp(message)
-	else:
-		print(base_message + "\t" + message)
+
+class Dprint:
+
+	def dp(self, message:[list, dict, str, int, bool] = None, debug_identifier:str = "Err: No Identifier set"):
+		now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S:")
+		base_message = f"\n[DEBUG]@[ {now}]>[{debug_identifier}])\n"
+		message = str(message)
+
+		if type(message) in ["list", "dict"]:
+			print(base_message)
+			pp(message)
+		else:
+			print(base_message + "\t" + message)
 
 if __name__ == "__main__":
-	dprint("Debugger Printer Module Test Message.", "Test")
+	Dprint.dp("Debugger Printer Module Test Message.", "Test")
