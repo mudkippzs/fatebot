7442b649cb2a538b5b48466bae0f63eb75301fbe
Mud <mudkippzs@gmail.com>
2022-02-17 16:30:02 +0000

    whitespace cleanup

--- dprint.py
+++ dprint.py
@@ -2,12 +2,11 @@ import argparse
 import pprint
 import datetime
 
-
 def dprint(message:[list, dict, str, int, bool] = None, debug_identifier:str = "Err: No Identifier set"):
 	now = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S:")
 	base_message = f"\n[DEBUG]@[ {now}]>[{debug_identifier}])\n"
 	message = str(message)
-	
+
 	if type(message) in ["list", "dict"]:
 		print(base_message)
 		pp(message)
